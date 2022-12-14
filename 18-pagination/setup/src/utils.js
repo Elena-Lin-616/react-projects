c18bf53f3d203afd77403953f3daf778e3ddeaca
Na Lin <lna483018@gmail.com>
2022-12-14 12:00:42 -0500

    pagination2: pagination logic

--- 18-pagination/setup/src/utils.js
+++ 18-pagination/setup/src/utils.js
@@ -1,3 +1,18 @@
-const paginate = () => {}
+const paginate = (followers) => {
+  // 0) get data from useFetch
+  // console.log(followers);
+  // 1) decide how many items per page
+  const itemsPerPage = 10;
 
-export default paginate
+  // 2) how many page do we have, alway round up
+  const pages = Math.ceil(followers.length / itemsPerPage);
+
+  // 3) create the array of array, with 10 elements = pages
+  const newFollower = Array.from({ length: pages }, (_, index) => {
+    const start = index * itemsPerPage;
+    return followers.slice(start, start + itemsPerPage);
+  });
+  return newFollower;
+};
+
+export default paginate;
